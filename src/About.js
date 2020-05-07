1570dc72f7a8cbbbf3baa4e123a59197bcf034f3
eminaydin <eminaydin93@yahoo.com>
2020-05-07 18:14:04 +0200

    Home page added

--- src/About.js
+++ src/About.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React from 'react'
+import { Feed, Container } from 'semantic-ui-react'
 
-const About = () => {
+const events = [
+    {
+        date: '1 Hour Ago',
+        image: 'https://react.semantic-ui.com/images/avatar/small/elliot.jpg',
+        meta: '4 Likes',
+        summary: 'Elliot Fu added you as a friend',
+    },
+    {
+        date: '4 days ago',
+        image: 'https://react.semantic-ui.com/images/avatar/small/helen.jpg',
+        meta: '1 Like',
+        summary: 'Helen Troy added 2 new illustrations',
+        extraImages: [
+            'https://react.semantic-ui.com/images/wireframe/image.png',
+            'https://react.semantic-ui.com/images/wireframe/image.png',
+        ],
+    },
+    {
+        date: '3 days ago',
+        image: 'https://react.semantic-ui.com/images/avatar/small/jenny.jpg',
+        meta: '8 Likes',
+        summary: 'Joe Henderson posted on his page',
+        extraText:
+            "Ours is a life of constant reruns. We're always circling back to where we'd we started.",
+    },
+    {
+        date: '4 days ago',
+        image: 'https://react.semantic-ui.com/images/avatar/small/jenny.jpg',
+        meta: '41 Likes',
+        summary: 'Justen Kitsune added 2 new photos of you',
+        extraText:
+            'Look at these fun pics I found from a few years ago. Good times.',
+        extraImages: [
+            'https://react.semantic-ui.com/images/wireframe/image.png',
+            'https://react.semantic-ui.com/images/wireframe/image.png',
+        ],
+    },
+    {
+        date: '1 Hour Ago',
+        image: 'https://react.semantic-ui.com/images/avatar/small/elliot.jpg',
+        meta: '4 Likes',
+        summary: 'Elliot Fu added you as a friend',
+    },
+    {
+        date: '4 days ago',
+        image: 'https://react.semantic-ui.com/images/avatar/small/helen.jpg',
+        meta: '1 Like',
+        summary: 'Helen Troy added 2 new illustrations',
+        extraImages: [
+            'https://react.semantic-ui.com/images/wireframe/image.png',
+            'https://react.semantic-ui.com/images/wireframe/image.png',
+        ],
+    },
+    {
+        date: '3 days ago',
+        image: 'https://react.semantic-ui.com/images/avatar/small/jenny.jpg',
+        meta: '8 Likes',
+        summary: 'Joe Henderson posted on his page',
+        extraText:
+            "Ours is a life of constant reruns. We're always circling back to where we'd we started.",
+    },
+    {
+        date: '4 days ago',
+        image: 'https://react.semantic-ui.com/images/avatar/small/jenny.jpg',
+        meta: '41 Likes',
+        summary: 'Justen Kitsune added 2 new photos of you',
+        extraText:
+            'Look at these fun pics I found from a few years ago. Good times.',
+        extraImages: [
+            'https://react.semantic-ui.com/images/wireframe/image.png',
+            'https://react.semantic-ui.com/images/wireframe/image.png',
+        ],
+    },
+]
 
-    return (
-        <p>sdfsdfds</p>
-    );
-}
-
-export default About;
+const About = () => <Feed events={events} />
+export default About
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
 import Projects from "./Projects"
 import About from "./About"
 import Nav from "./Nav"
-
+import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
 import "./App.css"
 
 export default function App() {
@@ -17,10 +17,21 @@ export default function App() {
       <h3>No match for <code>{location.pathname}</code></h3>
     </div>
   )
+
+  const photos = [];
+  for (let index = 0; index < 15; index++) {
+    photos.push(<Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />)
+
+  }
   return (
     <Router>
       <div>
         <Nav />
+        <Segment>
+          <Dimmer active>
+            <Loader size='massive'>This will probably take a loooong time</Loader>
+          </Dimmer>{photos}
+        </Segment>
         <Switch>
           <Route exact path="/" >
           </Route>
