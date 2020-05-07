a33b7460801a1d362f0ad6a62b705900c34166c0
eminaydin <eminaydin93@yahoo.com>
2020-05-07 17:52:33 +0200

    Dummy text added

--- src/Projects.js
+++ src/Projects.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Card, Header } from 'semantic-ui-react'
 const Projects = () => {
+    const src = 'https://react.semantic-ui.com/images/wireframe/image.png'
+    const style = {
+        margin: "20px",
+    }
     return (
-        <div>
-            Hello projects
-        </div>
+        <div className="projects">
+            <Header style={style}> Projects</Header>
+            <Card.Group itemsPerRow={4}>
+                <Card raised image={src} />
+                <Card raised image={src} />
+                <Card raised image={src} />
+                <Card raised image={src} />
+                <Card raised image={src} />
+                <Card raised image={src} />
+                <Card raised image={src} />
+                <Card raised image={src} />
+                <Card raised image={src} />
+                <Card raised image={src} />
+                <Card raised image={src} />
+                <Card raised image={src} />
+            </Card.Group>
+        </div >
     );
 }
 
