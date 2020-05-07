243097be5173d743f514d1ab8de9bc796f01a505
Emin <eminaydin93@yahoo.com>
2020-05-07 14:42:40 +0200

    Error on non-existing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import Projects from "./Projects"
 import About from "./About"
 import Nav from "./Nav"
+
 import "./App.css"
 
 export default function App() {
+
+  const NoMatch = ({ location }) => (
+    <div>
+      <h3>No match for <code>{location.pathname}</code></h3>
+    </div>
+  )
   return (
     <Router>
       <div>
         <Nav />
         <Switch>
-          <Route path="/" exact>
+          <Route exact path="/" >
           </Route>
           <Route path="/about">
             <About />
@@ -24,7 +30,8 @@ export default function App() {
           <Route path="/Projects">
             <Projects />
           </Route>
-
+          <Route component={NoMatch} >
+          </Route>
         </Switch>
       </div>
     </Router>
